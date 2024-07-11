03dac20962c22631dbd133b282cf940268378ee6
TehYongXian <yongxianteh@gmail.com>
2024-07-11 17:18:25 +0800

    done building the basic of the nav bar but still need to fix the details.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import Macbook from './views/Macbook';
+import NavBar from './views/NavBar';
 
 function App() {
   return (
     <Router>
-      <div className="app">
+      <div className="app ">
+        <div className="app p-5 border-solid border-b #D3D3D5">
+          <NavBar/>
+        </div>
           <Routes>
           <Route
             exact path="/" element={<Macbook />}
-              />
+            />
           </Routes>
         </div>
     </Router>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        '#D3D3D5':'#D3D3D5',
+        '#686869':'#686869',
+        '#0066CC' : '#0066CC',
+      }
+    },
+    
   },
   plugins: [],
 }
